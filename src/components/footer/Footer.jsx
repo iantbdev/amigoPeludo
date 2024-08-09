6f1b7474234562042288b51706ae60d78784a91f
Ian <familiadogutin@gmail.com>
2024-08-09 12:16:40 -0300

    style: ajuste no footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,38 +10,35 @@ import "./footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div className="col-md-4 d-flex align-items-center container">
-        <a
-          href="/home"
-          className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-        >
+    <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-0 border-top">
+      <div className="container">
+        <a href="/home" className="text-muted text-decoration-none lh-1">
           <img
             src={DogShield}
             alt="Logo"
-            style={{ width: "40px", height: "40px", marginRight: "10px" }}
+            style={{ width: "40px", height: "40px" }}
           />
         </a>
-        <span className="mb-3 mb-md-0 text-muted">©2024, Amigo Peludo</span>
-
-        <ul className="nav col-md-3 justify-content-center list-unstyled d-flex">
-          <li className="ms-0">
-            <a className="twitter" href="#">
-              <FontAwesomeIcon icon={faTwitter} size="2x" />
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="instagram" href="#">
-              <FontAwesomeIcon icon={faInstagram} size="2x" />
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="facebook" href="#">
-              <FontAwesomeIcon icon={faFacebook} size="2x" />
-            </a>
-          </li>
-        </ul>
+        <span className="text-muted">©2024, Amigo Peludo</span>
       </div>
+
+      <ul className="nav list-unstyled d-flex">
+        <li>
+          <a className="twitter" href="#">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+        </li>
+        <li>
+          <a className="instagram" href="#">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+        </li>
+        <li>
+          <a className="facebook" href="#">
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
